99e502544b2838ad3718574c82e541babd2628af
SantiagoGiraldoRodriguez <santi.arts2009@gmail.com>
2026-06-23 19:38:17 -0500

    correo confirmacion compra con resend

--- backend/src/utils/correo.js
+++ backend/src/utils/correo.js
@@ -19,7 +19,7 @@ const enviarCorreo = async ({ to, subject, html }) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      from: `"Moda Mágica" <${process.env.EMAIL_USER}>`,
+      from: 'Moda Mágica <onboarding@resend.dev>',
       to,
       subject,
       html,
